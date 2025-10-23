4f44e574c1cd1e30779833cdaaf55bd296359f34
marco47t <rasheedelouni@gmail.com>
2025-10-24 02:07:05 +0300

    Changing to receipt_bupload_message_handler

--- main.py
+++ main.py
@@ -500,7 +500,7 @@ def main():
     # ==========================
     application.add_handler(MessageHandler(
         filters.PHOTO | filters.Document.IMAGE, 
-        payment_handlers.handle_payment_receipt
+        payment_handlers.receipt_upload_message_handler
     ))
 
     application.add_handler(MessageHandler(
